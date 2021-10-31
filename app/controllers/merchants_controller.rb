38de31215c95dba40df0f739ff8eab926c6fd747
Tanner Dale <tanner@tannerdale.com>
2021-10-31 17:15:23 -0500

    Add page variable, response formatting, merchant fetching helper method

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,10 +1,22 @@
 class MerchantsController < ApplicationController
   def index
-    @merchants = MerchantFacade.merchants(params)
+    @page = params[:page]&.to_i || 1
+    @merchants = MerchantsSerializer.new(merchants).serializable_hash
+
+    respond_to do |f|
+      f.html
+      f.json { render json: { merchants: @merchants } }
+    end
   end
 
   def show
     @merchant = MerchantFacade.merchant(params[:id])
     @items = ItemFacade.items(params)
+  end
+
+  private
+
+  def merchants
+    MerchantFacade.merchants(params)
   end
 end
